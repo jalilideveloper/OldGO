cad4eae1665db43f8eb3b0e284fa3aac4bc90bcf
jalili.developer@gmail.com <jalili.developer@gmail.com>
2018-01-04 14:33:32 -0800

    شسیسشی

---
+++ old/GOWEB/Controllers/Error404Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GOWEB.Controllers
+{
+    public class Error404Controller : Controller
+    {
+        // GET: Error404
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //404
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        //403
+        public ActionResult BadRequest()
+        {
+            return View();
+        }
+
+        //500
+        public ActionResult ConstructoringInServer()
+        {
+            return View();
+        }
+
+        
+
+    }
+}
