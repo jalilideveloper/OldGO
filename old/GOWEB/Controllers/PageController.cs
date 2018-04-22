74c09c5583826f9881d73894311fcabc1b33275b
ash.shahbaz@yahoo.com <ash.shahbaz@yahoo.com>
2018-04-22 11:16:26 +0430

    xc

--- old/GOWEB/Controllers/PageController.cs
+++ old/GOWEB/Controllers/PageController.cs
@@ -20,5 +20,82 @@ namespace GOWEB.Controllers
         {
             return View();
         }
+
+        [ActionName("طراحی-سایت-پلان-دو")]
+        public ActionResult طراحی_سایت_پلان_دو()
+        {
+            return View();
+        }
+
+        [ActionName("چاپ-طراحی-سایت-پلان-دو")]
+        public ActionResult چاپ_طراحی_سایت_پلان_دو()
+        {
+            return View();
+        }
+
+        [ActionName("طراحی-سایت-پلان-سه")]
+        public ActionResult طراحی_سایت_پلان_سه()
+        {
+            return View();
+        }
+
+        [ActionName("چاپ-طراحی-سایت-پلان-سه")]
+        public ActionResult چاپ_طراحی_سایت_پلان_سه()
+        {
+            return View();
+        }
+
+        [ActionName("طراحی-سایت-پلان-چهار")]
+        public ActionResult طراحی_سایت_پلان_چهار()
+        {
+            return View();
+        }
+
+        [ActionName("چاپ-طراحی-سایت-پلان-چهار")]
+        public ActionResult چاپ_طراحی_سایت_پلان_چهار()
+        {
+            return View();
+        }
+
+        [ActionName("طراحی-سایت-پلان-پنج")]
+        public ActionResult طراحی_سایت_پلان_پنج()
+        {
+            return View();
+        }
+
+        [ActionName("چاپ-طراحی-سایت-پلان-پنج")]
+        public ActionResult چاپ_طراحی_سایت_پلان_پنج()
+        {
+            return View();
+        }
+
+        [ActionName("طراحی-سایت-پلان-شش")]
+        public ActionResult طراحی_سایت_پلان_شش()
+        {
+            return View();
+        }
+
+        [ActionName("چاپ-طراحی-سایت-پلان-شش")]
+        public ActionResult چاپ_طراحی_سایت_پلان_شش()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
